be9917f6c4710ce2b5cb97c47f005718ba53414a
Sravan Goud <tsravangoud164@gmail.com>
2025-01-24 21:19:20 +0530

    removed storyboard

---
+++ AniX/Modules/Home/View/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  AniX
+//
+//  Created by Sravan Goud on 24/01/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+}
